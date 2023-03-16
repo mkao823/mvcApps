3c86b80ac7a339821011253bf7d106c10a401b2c
mkao823 <99455489+mkao823@users.noreply.github.com>
2023-03-16 11:08:34 -0700

    Removed tools classes utilities, safeframe, and bean back into tools package, should be part of tools not mvc .

--- src/tools/Utilities.java
+++ src/tools/Utilities.java
@@ -1,79 +1,137 @@
 package tools;
 
-import java.awt.Component;
-import java.awt.event.ActionListener;
-import java.io.File;
-import javax.swing.JFileChooser;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+import java.awt.event.*;
+import java.io.*;
+import java.util.Random;
+import javax.swing.*;
 
 public class Utilities {
 
+    // asks user a yes/no question
     public static boolean confirm(String query) {
-        int result = JOptionPane.showConfirmDialog((Component)null, query, "Confirm", JOptionPane.OK_CANCEL_OPTION);
-        return result == JOptionPane.OK_OPTION;
+        int result = JOptionPane.showConfirmDialog(null,
+                query, "choose one", JOptionPane.YES_NO_OPTION);
+        return result == 0;
     }
 
+    // asks user for info
     public static String ask(String query) {
-        return JOptionPane.showInputDialog((Component)null, query);
+        return JOptionPane.showInputDialog(null, query);
     }
 
+    // tells user some info
     public static void inform(String info) {
-        JOptionPane.showMessageDialog((Component)null, info);
+        JOptionPane.showMessageDialog(null,info);
     }
 
+    // tells user a lot of info
     public static void inform(String[] items) {
         String helpString = "";
-
-        for(int i = 0; i < items.length; ++i) {
+        for(int i = 0; i < items.length; i++) {
             helpString = helpString + "\n" + items[i];
         }
-
         inform(helpString);
     }
 
+    // tells user about an error
     public static void error(String gripe) {
-        JOptionPane.showMessageDialog((Component)null, gripe, "OOPS!", 0);
+        JOptionPane.showMessageDialog(null,
+                gripe,
+                "OOPS!",
+                JOptionPane.ERROR_MESSAGE);
     }
 
+    // tells user about an exception
     public static void error(Exception gripe) {
         gripe.printStackTrace();
-        JOptionPane.showMessageDialog((Component)null, gripe.getMessage(), "OOPS!", 0);
+        JOptionPane.showMessageDialog(null,
+                gripe.getMessage(),
+                "OOPS!",
+                JOptionPane.ERROR_MESSAGE);
     }
 
-    public static JMenu makeMenu(String name, String[] items, ActionListener handler) {
-        JMenu result = new JMenu(name);
-
-        for(int i = 0; i < items.length; ++i) {
-            JMenuItem item = new JMenuItem(items[i]);
-            item.addActionListener(handler);
-            result.add(item);
-        }
-
-        return result;
+    // asks user to save changes
+    public static void saveChanges(Model model) {
+        if (model.getUnsavedChanges() && Utilities.confirm("current model has unsaved changes, continue?"))
+            Utilities.save(model, false);
     }
 
+    // asks user for a file name
     public static String getFileName(String fName, Boolean open) {
         JFileChooser chooser = new JFileChooser();
         String result = null;
         if (fName != null) {
+            // open chooser in directory of fName
             chooser.setCurrentDirectory(new File(fName));
         }
-
-        int returnVal;
         if (open) {
-            returnVal = chooser.showOpenDialog((Component)null);
-            if (returnVal == 0) {
-                result = chooser.getSelectedFile().getPath();
+            int returnVal = chooser.showOpenDialog(null);
+            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                result= chooser.getSelectedFile().getPath();
             }
         } else {
-            returnVal = chooser.showSaveDialog((Component)null);
-            if (returnVal == 0) {
-                result = chooser.getSelectedFile().getPath();
+            int returnVal = chooser.showSaveDialog(null);
+            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                result= chooser.getSelectedFile().getPath();
             }
         }
-
         return result;
     }
+
+    // save model
+    public static void save(Model model, Boolean saveAs) {
+        String fName = model.getFileName();
+        if (fName == null || saveAs) {
+            fName = getFileName(fName, false);
+            model.setFileName(fName);
+        }
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fName));
+            model.setUnsavedChanges(false);
+            os.writeObject(model);
+            os.close();
+        } catch (Exception err) {
+            model.setUnsavedChanges(true);
+            Utilities.error(err);
+        }
+    }
+
+    // open model
+    public static Model open(Model model) {
+        saveChanges(model);
+        String fName = getFileName(model.getFileName(), true);
+        Model newModel = null;
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream(fName));
+            newModel = (Model)is.readObject();
+            is.close();
+        } catch (Exception err) {
+            Utilities.error(err);
+        }
+        return newModel;
+    }
+
+    // simple menu maker
+    public static JMenu makeMenu(String name, String[] items, ActionListener handler) {
+        JMenu result = new JMenu(name);
+        for(int i = 0; i < items.length; i++) {
+            JMenuItem item = new JMenuItem(items[i]);
+            item.addActionListener(handler);
+            result.add(item);
+        }
+        return result;
+    }
+
+    // random number generator
+    public static Random rng = new Random(System.currentTimeMillis());
+
+    public static void log(String msg) {
+        System.out.println(msg); // for now
+    }
+
+    private static int nextID = 100;
+    public static int getID() {
+        return nextID++;
+    }
+
 }
