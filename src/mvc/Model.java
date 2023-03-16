2c57ed70e14bec13c92ff6bf1a9af041b18f8d47
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-03-15 21:21:19 -0700

    Update Model.java

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -1,4 +1,29 @@
 package mvc;
 
-public class Model {
+public class Model extends Bean {
+    boolean unsavedChanges;
+    String fileName;
+
+    public Model() {}
+
+    public void changed() {
+        unsavedChanges = true;
+        firePropertyChange(null, null,null);
+    }
+
+    public String getFileName(){
+        return fileName;
+    }
+
+    public void setFileName(String fName){
+        fileName = fName;
+    }
+
+    public boolean getUnsavedChanges(){
+        return unsavedChanges;
+    }
+
+    public void setUnsavedChanges(boolean i){
+        unsavedChanges = i;
+    }
 }
