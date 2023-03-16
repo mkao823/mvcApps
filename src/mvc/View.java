9bd74ad8ca2f4e8bc84cb566f2dcf1ea9b2d9ff2
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-03-15 21:18:35 -0700

    Update View.java

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,4 +1,24 @@
 package mvc;
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class View {
+public class View extends JPanel implements PropertyChangeListener {
+    private Model model;
+
+    public View(Model m) {
+        model = m;
+    }
+
+    public void setModel(Model newModel) {
+        model.removePropertyChangeListener(this);
+        model = newModel;
+        model.addPropertyChangeListener(this);
+    }
+
+    public void propertyChange(PropertyChangeEvent event) {
+
+    }
+
 }
