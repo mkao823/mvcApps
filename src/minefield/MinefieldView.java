19cb9d3cd99416d35ec3ab29fd34af78abb85c3e
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-03-19 16:01:05 -0700

    Update MinefieldView.java

--- src/minefield/MinefieldView.java
+++ src/minefield/MinefieldView.java
@@ -3,16 +3,19 @@ import mvc.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.beans.PropertyChangeEvent;
 
 // my MineField contains an n x n array of patches
+class Cell extends JLabel {
+    Patch patch;
 
+}
 
 public class MinefieldView extends View {
 
     private Cell cells[][];
     public MinefieldView(Minefield m) {
         super(m);
+        setBackground(Color.GRAY);
         init(m);
     }
 
@@ -25,54 +28,54 @@ public class MinefieldView extends View {
         setLayout(new GridLayout(dim, dim));
         for(int row = 0; row < dim; row++) {
             for (int col = 0; col < dim; col++) {
-                // create and initialize cells here, use
                 cells[row][col] = new Cell();
                 cells[row][col].patch = m.getPatches(row, col);
-                cells[row][col].setBorder(BorderFactory.createLineBorder(Color.black));
-                //gc.drawRect(20 * row, 20 * col, 20, 20);
                 if(cells[row][col].patch.occupied){
-                    cells[row][col].setBackground(Color.blue);
-                    cells[row][col].setBorder(BorderFactory.createLineBorder(Color.blue));
+                    if(row == m.getLocationX() && col == m.getLocationY()){
+                        gc.setColor(Color.BLUE);
+                        gc.drawRect(dim * row + 4, dim * col + 5, dim - 3, dim - 3);
+                    }
+                    else {
+                        gc.setColor(Color.WHITE);
+                        gc.drawRect(dim * row + 4, dim * col + 5, dim - 3, dim - 3);
+                    }
+                    gc.setColor(Color.BLACK);
+                    gc.drawString("" + cells[row][col].patch.surroundingMines,
+                            (dim * row) + dim / 3,(dim * (col + 1)));
                 }
+                else {
+                    gc.setColor(Color.black);
+                    gc.drawString("?", (dim * row) + dim / 3,(dim * (col + 1)) );
+                }
+
+                if(cells[row][col].patch.mine){
+                    gc.setColor(Color.magenta);
+                    gc.drawRect(dim * row + 4, dim * col + 5, dim - 3, dim - 3);
+                }
+
                 if(cells[row][col].patch.goal){
-                    cells[row][col].setBackground(Color.green);
-                    cells[row][col].setBorder(BorderFactory.createLineBorder(Color.green));
+                    gc.setColor(Color.GREEN);
                 }
             }
         }
         gc.setColor(oldColor);
     }
-    public void init(Minefield m){
+
+    public void init(Minefield m) {
         int dim = m.getHeight();
         cells = new Cell[dim][dim];
         setLayout(new GridLayout(dim, dim));
         for(int row = 0; row < dim; row++) {
             for (int col = 0; col < dim; col++) {
-                // create and initialize cells here, use
                 cells[row][col] = new Cell();
                 cells[row][col].patch = m.getPatches(row, col);
                 cells[row][col].setBorder(BorderFactory.createLineBorder(Color.black));
-                if(cells[row][col].patch.occupied){
-                    cells[row][col].setBackground(Color.blue);
-                    cells[row][col].setBorder(BorderFactory.createLineBorder(Color.blue));
-                }
-                if(cells[row][col].patch.goal){
-                    cells[row][col].setBackground(Color.green);
-                    cells[row][col].setBorder(BorderFactory.createLineBorder(Color.green));
-                }
-                if(cells[row][col].patch.mine){
-                    cells[row][col].setBackground(Color.red);
-                    cells[row][col].setBorder(BorderFactory.createLineBorder(Color.red));
+                if(cells[row][col].patch.goal) {
+                    cells[row][col].setBackground((Color.GREEN));
+                    cells[row][col].setBorder(BorderFactory.createLineBorder(Color.GREEN));
                 }
                 add(cells[row][col]);
             }
-        }
-    }
-    class Cell extends JLabel {
-        Patch patch;
-
-        public void setPatch(Patch patches) {
-            this.patch = patches;
         }
     }
 }
