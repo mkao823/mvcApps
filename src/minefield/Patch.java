b2a35f67ea9003c8536b4bd77c082ec2413a78fc
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-03-19 15:48:19 -0700

    added increamentSurMines()

--- src/minefield/Patch.java
+++ src/minefield/Patch.java
@@ -5,4 +5,8 @@ public class Patch {
     public boolean goal = false; //initial value of goal is false, should only have 1 goal in the 2d array
     public int surroundingMines = 0; //number of mines around this patch
     public boolean occupied = false; //set to true when player moves to the patch(Minefield.move(Heading))
+    
+    public void increamentSurMines() {
+        surroundingMines++;
+    }
 }
