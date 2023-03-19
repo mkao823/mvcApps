03a1d01d918557eda121fc2edeb1eb9d786a55fe
Jiahao Ye <52772841+haorino@users.noreply.github.com>
2023-03-18 22:44:24 -0700

    Update MinefieldView.java

--- src/minefield/MinefieldView.java
+++ src/minefield/MinefieldView.java
@@ -1,21 +1,40 @@
 package minefield;
-
 import mvc.*;
 
+import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+
+// my MineField contains an n x n array of patches
+class Cell extends JLabel {
+    Patch patch;
+
+    public void setPatch(Patch patches) {
+        this.patch = patches;
+    }
+}
 
 public class MinefieldView extends View {
 
-    public MinefieldView(Model mine) {
-        super(mine);
+    private Cell cells[][];
+    public MinefieldView(Minefield m) {
+        super(m);
     }
 
-    /*public void paintComponent(Graphics gc) {
+    public void paintComponent(Graphics gc) {
         super.paintComponent(gc);
+        Minefield m = (Minefield) model;
         Color oldColor = gc.getColor();
-        Model mod = (Minefield)model;
-        Minefield shape = new Minefield(mod);
-        shape.draw((Graphics2D) gc);
+        int dim = m.getHeight();
+        cells = new Cell[dim][dim];
+        setLayout(new GridLayout(dim, dim));
+        for(int row = 0; row < dim; row++) {
+            for (int col = 0; col < dim; col++) {
+                // create and initialize cells here, use
+                cells[row][col] = new Cell();
+                cells[row][col].setBorder(BorderFactory.createLineBorder(Color.black));
+            }
+        }
         gc.setColor(oldColor);
-    }*/
+    }
 }
